b5e9a18be10fd90ab585faab0929957ccb3143f8
huntermacias <supercodersf@gmail.com>
2024-01-13 19:00:57 -0800

    data: updated images on home pages

--- src/images/travel/index.ts
+++ src/images/travel/index.ts
@@ -1,115 +1,80 @@
-import antelope from './antelope.jpg';
-import bura from './bura.jpg';
-import image10 from './canaries.jpeg';
-import image11 from './crete.jpeg';
-import croatia2 from './croatia2.jpg';
-import croatia3 from './croatia3.jpg';
-import image3 from './croatia.jpeg';
-import croatia from './croatia.jpg';
-import image12 from './czechia.jpeg';
-import image5 from './france.jpeg';
-import image7 from './germany.jpeg';
-import grandcanyon from './grandcanyon.jpg';
-import griffith from './griffith.jpg';
-import grindelwald from './grindelwald.jpg';
-import guns from './guns.jpeg';
-import image6 from './italy.jpeg';
-import jungfraujoch2 from './jungfraujoch2.jpg';
-import lasvegas2 from './lasvegas2.jpeg';
-import lasvegas from './lasvegas.jpg';
-import lauterbrunnen from './lauterbrunnen.jpg';
-import lucerne from './lucerne.jpg';
-import matterhorn2 from './matterhorn2.jpg';
-import matterhorn from './matterhorn.jpg';
-import image13 from './mexico.jpeg';
-import image14 from './monaco.jpeg';
-import image4 from './netherlands.jpeg';
-import paros from './paros.jpg';
-import poland2 from './poland2.jpg';
-import poland from './poland.jpg';
-import sedona from './sedona.jpg';
-import image9 from './spain.jpeg';
-import image2 from './switzerland.jpeg';
-import image8 from './uk.jpeg';
-import image1 from './usa.jpeg';
-import zabkowice from './zabkowice.jpg';
-import zurich from './zurich.jpg';
+import image1 from './1.jpg';
+import image2 from './2.jpg';
+import image3 from './3.jpg';
+import image4 from './4.jpg';
+import image5 from './5.jpg';
+import image6 from './6.jpg';
+import image7 from './7.jpg';
+import image8 from './8.jpg';
+import image9 from './9.jpg';
+import image10 from './10.jpg';
+import image11 from './11.jpg';
+import image12 from './12.jpg';
+import image13 from './13.jpg';
+import image14 from './14.jpg'; 
+import image15 from './15.jpg';
+import image16 from './16.jpg';
+import image17 from './17.jpg';
+import image18 from './18.jpg';
+import image19 from './19.jpg';
+import image20 from './20.jpg';
+import image21 from './21.jpg';
+import image22 from './22.jpg';
 
 export const travelImages = [
-  { img: image1, title: 'Horseshoe Bend, USA', alt: 'Picture taken in Horseshoe, USA' },
-  { img: antelope, title: 'Antelope Canyon, USA', alt: 'Picture taken in Antelope Canyon, USA' },
-  { img: grandcanyon, title: 'Grand Canyon, USA', alt: 'Picture taken in Grand Canyon, USA' },
-  { img: sedona, title: 'Sedona, USA', alt: 'Picture taken in Sedona, USA' },
-  { img: lasvegas, title: 'Las Vegas, USA', alt: 'Picture taken in Las Vegas, USA' },
-  { img: lasvegas2, title: 'Las Vegas, USA', alt: 'Picture taken in Las Vegas, USA' },
+  { img: image1, title: 'Free photobooth, USA', alt: 'Picture taken in Seattle, USA' },
+  { img: image2, title: 'Mac Miller Light Show, USA', alt: 'Picture taken Pacific Science Center, USA' },
+  { img: image3, title: 'Am I an adult?, USA', alt: 'Picture taken in the car, USA' },
+  { img: image4, title: 'Allegiant Stadium, USA', alt: 'Picture taken in Las Vegas, USA' },
+  { img: image5, title: 'Stranger Things Exhibit, USA', alt: 'Picture taken in Las Vegas, USA' },
+  { img: image6, title: 'Las Vegas, USA', alt: 'Picture taken in Las Vegas, USA' },
   {
-    img: griffith,
-    title: 'Griffith Observatory, USA',
-    alt: 'Picture taken in Griffith Observatory, USA',
+    img: image7,
+    title: 'Personality Shot, USA',
+    alt: 'Picture taken at Vodoo Donuts in Portland Oregon',
   },
-  { img: poland, title: 'Wrocław, Poland', alt: 'Picture taken in Wrocław, Poland' },
-  { img: poland2, title: 'Ustronie, Poland', alt: 'Picture taken in Ustronie, Poland' },
-  { img: zabkowice, title: 'Zabkowice, Poland', alt: 'Picture taken in Zabkowice, Poland' },
+  { img: image8, title: 'Washington State Fair', alt: 'Washington State Fair' },
+  { img: image9, title: 'Why so much milk?', alt: 'Picture taken in Seattle, WA' },
+  { img: image10, title: 'First Rainy bike ride', alt: 'Picture taken in Seattle, WA' },
   {
-    img: guns,
-    title: "Guns n' Roses Concert, Warsaw",
-    alt: "Picture taken Guns n' Roses Concert, Warsaw",
+    img: image11,
+    title: "lucholibre Championship",
+    alt: "Seattle Washington",
   },
-  { img: bura, title: 'Bura, Wrocław, Poland', alt: 'Picture of my cat, Bura' },
+  { img: image12, title: 'Bura, Wrocław, Poland', alt: 'Picture of my cat, Bura' },
   {
-    img: croatia,
-    title: 'Krka-Waterfalls, Croatia',
+    img: image13,
+    title: 'Gorgeous Music Festival',
+    alt: 'Music Festival',
+  },
+  {
+    img: image14,
+    title: 'Van gogh Museum',
     alt: 'Picture taken in Krka-Waterfalls, Croatia',
   },
+  { img: image15, title: 'Dubrovnik, Croatia', alt: 'Picture taken in Dubrovnik, Croatia' },
   {
-    img: croatia2,
-    title: 'Krka-Waterfalls, Croatia',
-    alt: 'Picture taken in Krka-Waterfalls, Croatia',
-  },
-  { img: image3, title: 'Dubrovnik, Croatia', alt: 'Picture taken in Dubrovnik, Croatia' },
-  {
-    img: croatia3,
-    title: 'Sadurad Island, Croatia',
+    img: image16,
+    title: 'Lone Mountain, USFCA',
     alt: 'Picture taken in Sadurad Island, Croatia',
   },
-  { img: paros, title: 'Paros, Greece', alt: 'Picture taken in Paros, Greece' },
+  { img: image17, title: 'Paros, Greece', alt: 'Picture taken in Paros, Greece' },
   {
-    img: image2,
-    title: 'Jungfraujoch, Switzerland',
+    img: image18,
+    title: 'Greek Champions',
     alt: 'Picture taken on Jungraujoch, Switzerland',
   },
   {
-    img: jungfraujoch2,
-    title: 'Jungfraujoch, Switzerland',
+    img: image19,
+    title: 'Downtown San Francisco',
     alt: 'Picture taken on Jungraujoch, Switzerland',
   },
-  { img: zurich, title: 'Zurich, Switzerland', alt: 'Picture taken in Zurich, Switzerland' },
-  { img: lucerne, title: 'Lucerne, Switzerland', alt: 'Picture taken in Lucerne, Switzerland' },
+  { img: image20, title: 'My first time in snow', alt: 'Picture taken in Zurich, Switzerland' },
+  { img: image21, title: 'Family', alt: 'Picture taken in Lucerne, Switzerland' },
   {
-    img: lauterbrunnen,
-    title: 'Lauterbrunnen, Switzerland',
+    img: image22,
+    title: 'St Louis, Missouri',
     alt: 'Picture taken in Lauterbrunnen, Switzerland',
   },
-  { img: matterhorn, title: 'Zermatt, Switzerland', alt: 'Picture taken in Zermatt, Switzerland' },
-  { img: matterhorn2, title: 'Zermatt, Switzerland', alt: 'Picture taken in Zermatt, Switzerland' },
-  {
-    img: grindelwald,
-    title: 'Grindelwald, Switzerland',
-    alt: 'Picture taken in Grindelwald, Switzerland',
-  },
-  { img: image4, title: 'Amsterdam, Netherlands', alt: 'Picture taken in Amsterdam, Netherlands' },
-  { img: image5, title: 'Grasse, France', alt: 'Picture taken in Grasse, France' },
-  { img: image6, title: 'Portofino, Italy', alt: 'Picture taken in Rome, Italy' },
-  { img: image7, title: 'Berlin, Germany', alt: 'Picture taken in Berlin, Germany' },
-  { img: image8, title: 'London, UK', alt: 'Picture taken in London, UK' },
-  { img: image9, title: 'Barcelona, Spain', alt: 'Picture taken in Barcelona, Spain' },
-  {
-    img: image10,
-    title: 'Fuerteventura, Canary Islands',
-    alt: 'Picture taken in Fuerteventura, Canary Islands',
-  },
-  { img: image11, title: 'Crete, Greece', alt: 'Picture taken in Crete, Greece' },
-  { img: image12, title: 'Prague, Czechia', alt: 'Picture taken in Prague, Czechia' },
-  { img: image13, title: 'Merida, Mexico', alt: 'Picture taken in Merida, Mexico' },
-  { img: image14, title: 'Monaco', alt: 'Picture taken in Monaco' },
+  
 ];
