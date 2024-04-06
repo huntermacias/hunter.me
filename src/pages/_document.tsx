0077b202db48996c9f470f36e7f534d34af6049e
Hunter Macias <huntermacias@Hunters-MacBook-Pro.local>
2024-04-06 16:30:22 -0700

    updated editor theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,11 +14,11 @@ export default function Document() {
         {/* Add these tags for social media previews */}
         <meta property="og:title" content="Hunter Macias: Innovative Software Engineer & Tech Problem Solver" />
         <meta property="og:description" content="Dive into the portfolio of Hunter Macias, a passionate software engineer with a knack for tackling complex tech challenges. Discover a blend of cutting-edge projects showcasing expertise in AI, full-stack development, and cloud solutions. John's portfolio is not just a display of technical skills but a testament to a continuous journey of learning, innovation, and driving impact in the tech world." />
-        <meta name="image" property="og:image" content="https://i.imgur.com/UJUYMh8.png" />
+        <meta name="image" property="og:image" content="/11.png" />
         <meta name="author" content="Hunter Macias"></meta>
         <meta property="og:url" content="https://hunter-me.vercel.app" />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="VisionCode:HM" />
+        <meta property="og:site_name" content="HunterMacias" />
 
         {/* Twitter Card tags */}
         <meta name="twitter:card" content="summary_large_image" />
