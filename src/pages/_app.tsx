a4980f19cfa91e02d116db3dd326c91d8b09c31f
huntermacias <supercodersf@gmail.com>
2026-08-09 20:11:01 -0700

    added speed analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import 'focus-visible';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
